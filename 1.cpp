9d5caec470c3673b5fb3af0716d2fdd6cd316322
asfrzn181 <61591815+asfrzn181@users.noreply.github.com>
2020-03-08 06:55:09 -0700

    Nh3ntai Gacha Code
    I don't know what I'am Doing

---
+++ 1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <conio.h>
+#include <stdlib.h>
+using namespace std;
+
+int main(){
+    system("color 2");
+while(true){
+cout << "nhentai.net/g/";
+for (int x=0;x<=5;x++){
+    cout << rand()%10;
+}
+cout << endl;
+getch();
+}
+
+}
